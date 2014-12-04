0bff16863aa7dceef13b3eae3e9cc9d0cd2b58b4
xmichaelx <michal.drzal@gmail.com>
2014-12-04 19:52:37 +0100

    now one script generates one big graph

--- generate_big_graph.py
+++ generate_big_graph.py
@@ -13,29 +13,34 @@ if len(sys.argv) < 2 or not os.path.exists(sys.argv[1]):
 	print("You need to specify a valid folder with graphs")
 else:
 	filenames = [sys.argv[1] + "/" + x for x in os.listdir(sys.argv[1]) if x.startswith("arxiv_") and x.endswith(".json")]
+	auths = set()
 	pairs = {}
+	print("Retrieving nodes and edges")
 	for filename in filenames:
 		print(filename)
 		with open(filename) as f:
 			chunk = json.load(f)
 			for article in chunk:
 				authors = [x["id"] for x in article["authors"]]
+
+				for author in authors:
+					auths.add(author)
+
 				for pair in itertools.combinations_with_replacement(authors, 2):
 					add_edge(pairs, pair[0], pair[1])
+				
 
-	print("Opened nodes")
-	with open("nodes.json") as nodes_file:
-		nodes = json.load(nodes_file)
+	auths = list(auths)
+
+	with open('nodes.json', "w") as ofile:
+		json.dump(auths, ofile, indent=4)
 
 	print("Transforming to dict")
-	node_dict = { x: i for i,x in enumerate(nodes) }
+	node_dict = { x: i for i,x in enumerate(auths) }
+	auths = None
 
 	print("Writing")
 	with open('edges.csv',"w") as fp:
 		for key in pairs:
 			line = str(node_dict[key[0]]) + " " + str(node_dict[key[1]]) +" " + str(pairs[key])
 			print(line, file = fp )
-
-	with open('edges.json', "w") as f:
-		json.dump(edges, f, indent=4)
-	
