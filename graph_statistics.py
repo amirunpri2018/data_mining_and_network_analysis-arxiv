6228508be025c64c253918b380cb22c457bf974f
xmichaelx <michal.drzal@gmail.com>
2014-12-05 08:51:53 +0100

    eigen vector centrality nodes added

--- graph_statistics.py
+++ graph_statistics.py
@@ -1,6 +1,9 @@
+from __future__ import print_function
+from collections import Counter
 __author__ = 'Michal Drzal'
 
-import snap
+import snap,json
+
 
 # requires python 2.7 and 64-bit machine
 G1 = snap.TUNGraph.New()
@@ -16,7 +19,29 @@ with open("data/edges.csv") as f:
 
 		G1.AddEdge(n1, n2)
 
-print "Stats"
-snap.PrintInfo(G1, "arXiv netwrok", "stats.txt", False)
+
+# code to get strongly connected components
+Components = snap.TCnComV()
+snap.GetSccs(G1, Components)
+
+cnt = Counter([CnCom.Len() for CnCom in Components])
+
+print(cnt.most_common())
+
+with open("statistics/component_size_distribution.csv","w") as f:
+	for item in cnt.most_common():
+		print(str(item[0]) + " " + str(item[1]), file=f)
+
+# #code to generate node degree distribution
+#DegToCntV = snap.TIntPrV()
+#snap.GetDegCnt(G1, DegToCntV)
 
 
+#degrees =[]
+#counts = []
+
+#with open("statistics/node_degree_distribution.csv","w") as f:
+#	for item in DegToCntV:
+#		degrees.append(float(item.GetVal1()))
+#		counts.append(float(item.GetVal2()))
+#		print(str(item.GetVal1()) + " " + str(item.GetVal2()), file=f)
